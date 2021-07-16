7e6bbc9f5c9fe4dce091efec2177d4f6a0dddf9c
Moyi Tian <68933512+TDuoduo97@users.noreply.github.com>
2021-07-16 08:18:21 -0400

    Add files via upload

---
+++ symmetric_coupled_ring/ring_net_animation/LLCHom_states/LDS_RHS.m
@@ -0,0 +1,16 @@
+function [F,J] = LDS_RHS(u,p,C)
+  
+% rename parameters
+d = p(1);
+mu = p(2);
+N = size(u,1);
+
+% right-hand side
+F = d*C*u -mu*u + 2*u.^3 - u.^5;
+
+% Jacobian
+if nargout > 1
+	J = d*C + spdiags(-mu + 6*u.^2 - 5*u.^4, 0, N, N);
+end
+
+end
